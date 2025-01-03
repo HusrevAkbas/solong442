0c851de5cd591d258c1e5dfa9d226f873a7b7911
Husrev Akbas <huakbas@c2r3p1.42vienna.com>
2025-01-03 17:21:01 +0100

    play with images

--- image.c
+++ image.c
@@ -6,7 +6,7 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 11:36:00 by huakbas           #+#    #+#             */
-/*   Updated: 2025/01/03 16:16:38 by huakbas          ###   ########.fr       */
+/*   Updated: 2025/01/03 17:17:19 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,33 @@ void	set_images(t_screen *screen)
 	t_image	sprite;
 	t_image	background;
 	char	*add;
+	char	*add2sp;
+	int		color = 16711680;
 	background.img = mlx_new_image(screen->mlx, 200, 200);
 	background.address = mlx_get_data_addr(background.img, &background.bits_per_pixel, &background.line_length, &background.endian);
-	ft_printf("len: %i, bits: %i, endian: %i\n", background.line_length, background.bits_per_pixel, background.endian);
 	sprite.img = mlx_xpm_file_to_image(screen->mlx, "assets/Eyemonster.xpm", &sprite.width, &sprite.heigth);
 	if (!sprite.img)
 		return ;
+	sprite.address = mlx_get_data_addr(sprite.img, &sprite.bits_per_pixel, &sprite.line_length, &sprite.endian);
+	ft_printf("value of x 0 y 0: %c\n", *sprite.address);
 	int x = 0;
 	int y = 0;
 	while (x < 200)
 	{
 		y = 0;
+		color = 16711680;
 		while (y < 200)
 		{
 			add = background.address + (y * background.line_length + x * (background.bits_per_pixel / 8));
-			*(unsigned int *)add = 0xFF0000;
+			if (y < sprite.heigth)
+			{
+				add2sp = sprite.address + (y * sprite.line_length + x * (sprite.bits_per_pixel / 8));
+				if (*add2sp == 0 && *(add2sp + sprite.bits_per_pixel / 8) == 0)
+					*(unsigned int *)add2sp = color;
+			}
+			*(unsigned int *)add = color;
 			y++;
+			color -= 2;
 		}
 		x++;
 	}
